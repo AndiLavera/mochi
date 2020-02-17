f852e39678454982d2762ee02247a5c1a9ff7ee3
andrewc910 <awcrotwell@gmail.com>
2020-02-17 10:37:32 -0800

    Vk provider is now working, test is stable

---
+++ spec/mochi/omniauthable/providers/vk_spec.cr
@@ -0,0 +1,14 @@
+require "../../../spec_helper"
+
+VK_RAW_JSON = "{\"id\":1047667689746652,\"name\":\"con trive\",\"last_name\":\"trive\",\"first_name\":\"con\",\"email\":\"vktest\\u0040gmail.com\"}"
+
+describe Mochi::Omniauthable::Provider::Vk do
+  it "should return instance of FbUser with mapped values" do
+    fakeuser = Mochi::Omniauthable::Provider::Vk::VkUser.from_json(VK_RAW_JSON)
+    fakeuser.class.should eq(Mochi::Omniauthable::Provider::Vk::VkUser)
+
+    fakeuser.name.should eq("con trive")
+    fakeuser.provider.should eq("vk")
+    fakeuser.uid.should eq("1047667689746652")
+  end
+end
