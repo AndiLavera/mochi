78bbf976ac50f31bce700fc00d1a87b6408a3b41
Andrew <awcrotwell@gmail.com>
2020-03-04 08:28:32 -0800

    Actually fixed broken tests

--- src/mochi/mailers/mochi_mailer.cr
+++ src/mochi/mailers/mochi_mailer.cr
@@ -2,4 +2,5 @@ abstract class Mochi::Mailer
   abstract def confirmation_instructions
   abstract def reset_password_instructions
   abstract def unlock_instructions
+  abstract def invitation_instructions
 end
