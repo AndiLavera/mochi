e057cb13a5795728a72167fab394181c318592d6
Andrew <awcrotwell@gmail.com>
2020-04-09 10:18:41 -0700

    Postgres db for tests

--- spec/support/models.cr
+++ spec/support/models.cr
@@ -1,5 +1,7 @@
 # User class for testing Jennifer ORM
 class JenniferUser < Jennifer::Model::Base
+  @password : String?
+  def password; @password; end
   include Mochi
   mochi_jennifer(
     :authenticable,
@@ -14,14 +16,14 @@ class JenniferUser < Jennifer::Model::Base
 
   with_timestamps
   mapping(
-    id: {type: Int64, primary: true},
+    id: {type: Int32, primary: true},
     created_at: {type: Time, null: true},
     updated_at: {type: Time, null: true},
     email: {type: String, default: ""},
     password_digest: {type: String?},
     confirmation_token: {type: String?, null: true},
-    confirmed: {type: Bool, null: true},
-    confirmed_at: {type: Time, null: true},
+    confirmed: {type: Bool, default: false},
+    confirmed_at: {type: Time?},
     uncomfirmed_email: {type: String?},
     confirmation_sent_at: {type: Time?},
     uid: {type: String?},
@@ -32,14 +34,14 @@ class JenniferUser < Jennifer::Model::Base
     last_sign_in_at: {type: Time?, null: true},
     reset_password_sent_at: {type: Time?},
     reset_password_token: {type: String?},
-    password_reset_in_progress: {type: Bool, default: false},
+    password_reset_in_progress: {type: Bool?, default: false},
     locked_at: {type: Time?},
     failed_attempts: {type: Int32, default: 0, null: false},
     unlock_token: {type: String?},
     invitation_accepted_at: {type: Time?},
     invitation_created_at: {type: Time?},
     invitation_token: {type: String?},
-    invited_by: {type: Int64?},
+    invited_by: {type: Int32?},
     invitation_sent_at: {type: Time?},
   )
 end
@@ -47,6 +49,8 @@ end
 # User class for testing Granite ORM
 # Should be identical to JenniferUser
 class User < Granite::Base
+  @password : String?
+  def password; @password; end
   include Mochi
   mochi_granite(
     :authenticable,
@@ -59,14 +63,14 @@ class User < Granite::Base
   )
   include Mochi::Models::Authenticable::Validations::Granite
 
-  connection sqlite
+  connection postgres
   table jennifer_users
 
-  column id : Int64, primary: true
+  column id : Int32, primary: true
   column email : String?
   column password_digest : String?
   column confirmation_token : String?
-  column confirmed : Bool = false
+  column confirmed : Bool? = false
   column confirmed_at : Time?
   column confirmation_sent_at : Time?
   column uncomfirmed_email : String?
@@ -78,7 +82,7 @@ class User < Granite::Base
   column last_sign_in_at : Time?
   column reset_password_sent_at : Time?
   column reset_password_token : String?
-  column password_reset_in_progress : Bool = false
+  column password_reset_in_progress : Bool? = false
   column locked_at : Time?
   column failed_attempts : Int32 = 0
   column unlock_token : String?
