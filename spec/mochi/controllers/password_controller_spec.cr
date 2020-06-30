9adc16ebbe3c5b536a64b299348bb9c9b3edbeaf
Andrew <awcrotwell@gmail.com>
2020-06-30 16:50:37 -0700

    Completed password controller spec

--- spec/mochi/controllers/password_controller_spec.cr
+++ spec/mochi/controllers/password_controller_spec.cr
@@ -9,7 +9,7 @@ require "../../spec_helper"
 
     it "should not find user and give error message" do
       context = build_get_request("/")
-      controller_class.new(context).create(nil).should be_true
+      controller_class.new(context).create(nil)
       context.flash[:danger].should eq("Could not find user with that email.")
     end
 
@@ -26,6 +26,44 @@ require "../../spec_helper"
       usr.reset_password_token.should_not be_nil
       usr.password_reset_in_progress.should be_true
       usr.reset_password_sent_at.should_not be_nil
+    end
+
+    it "should complete a password reset without signing in" do
+      Mochi.configuration.sign_in_after_reset_password = false
+
+      email = "pc0_test#{UUID.random}@email.com"
+      usr = User.build!({
+        email:    email,
+        password: "Password123",
+      })
+      usr.send_reset_password_instructions
+
+      context = build_post_request("/?reset_token=#{usr.reset_password_token}")
+      controller_class.new(context).update(usr)
+
+      usr.reset_password_token.should be_nil
+      usr.reset_password_sent_at.should be_nil
+      usr.password_reset_in_progress.should be_false
+      context.session[:user_id].should be_nil
+    end
+
+    it "should complete a password reset with signing in" do
+      Mochi.configuration.sign_in_after_reset_password = true
+
+      email = "pc0_test#{UUID.random}@email.com"
+      usr = User.build!({
+        email:    email,
+        password: "Password123",
+      })
+      usr.send_reset_password_instructions
+
+      context = build_post_request("/?reset_token=#{usr.reset_password_token}")
+      controller_class.new(context).update(usr)
+
+      usr.reset_password_token.should be_nil
+      usr.reset_password_sent_at.should be_nil
+      usr.password_reset_in_progress.should be_false
+      context.session[:user_id].should_not be_nil
     end
   end
 end
