afeab7fb6b124d34d5bc9aa568c8997b0a96d946
Andrew <awcrotwell@gmail.com>
2020-06-30 17:11:25 -0700

    Registration spec

--- src/mochi/controllers/registration_controller.cr
+++ src/mochi/controllers/registration_controller.cr
@@ -1,11 +1,11 @@
 class Mochi::Controllers::RegistrationController < Mochi::Controllers::ApplicationController
   def confirm(user)
-    return redirect_to "/", flash: {"error" => "Invalid authenticity token."} if user.nil?
+    return redirect_to "/", flash: {"danger" => "Invalid authenticity token."} if user.nil?
 
     if user.confirm! && user.save
       redirect_to "/", flash: {"success" => "User has been confirmed."}
     else
-      redirect_to "/", flash: {"error" => "Token has expired."}
+      redirect_to "/", flash: {"danger" => "Token has expired."}
     end
   end
 
