5c12cb9c86784845896c09550bd6e9890db9b7ec
Andrew <awcrotwell@gmail.com>
2020-07-02 13:23:47 -0700

    Update confirmable spec

--- spec/mochi/models/confirmable_spec.cr
+++ spec/mochi/models/confirmable_spec.cr
@@ -10,6 +10,7 @@ describe Mochi::Models::Confirmable do
     end
 
     it "should confirm user for #{name_formatter(user_class)}" do
+      Mochi.configuration.confirm_within = 7
       user = user_class.new
       user.email = "co0_test#{rand(0..500)}@email.com"
       user.password = "password123"
@@ -23,6 +24,7 @@ describe Mochi::Models::Confirmable do
     end
 
     it "should not confirm user for #{name_formatter(user_class)}" do
+      Mochi.configuration.confirm_within = 7
       user = user_class.new
       user.email = "co1_test#{rand(0..500)}@email.com"
       user.password = "password123"
