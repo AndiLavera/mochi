58b7ecb69bbe55c381e184640f7a5db2ea97ffb3
Andrew <awcrotwell@gmail.com>
2020-07-03 15:18:51 -0700

    Updated spec mailer

--- spec/support/mailers/mailer.cr
+++ spec/support/mailers/mailer.cr
@@ -1,27 +1,9 @@
 class Mochi::DefaultMailer < Mochi::Mailer
-  # include Mochi::Helpers::Mailer
-
-  def confirmation_instructions(record : User | JenniferUser, token : String, *opts)
-    true
-  end
-
-  def reset_password_instructions(record : User | JenniferUser, token : String, *opts)
-    true
-  end
-
-  def unlock_instructions(record : JenniferUser | User, token : String, *opts)
-    true
-  end
-
-  # def email_changed(record, opts : Hash(String, String))
-  #   mochi_mail(record, :email_changed, opts)
-  # end
-
-  # def password_change(record, opts : Hash(String, String))
-  #   mochi_mail(record, :password_change, opts)
-  # end
-
-  def invitation_instructions(record : JenniferUser | User, token : String, *opts)
-    true
-  end
+  {% for klass in USER_CLASSES %}
+    {% for i in ["confirmation", "reset_password", "unlock", "invitation"] %}
+      def {{i.id}}_instructions(record : {{klass}}, token : String, *opts)
+        true
+      end
+    {% end %}
+  {% end %}
 end
