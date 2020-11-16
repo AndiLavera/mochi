f5227fc0cf3cab99d64cd2901883ad8f090ccdc8
Andrew <49383564+andrewc910@users.noreply.github.com>
2020-11-16 16:45:38 -0600

    Add support for Amber plugin system (#34)
    * Begin moving template files into plugin folder
    
    * Authenticable plugin
    
    * README
    
    * Moved files around, configured config.yml
    
    * Amber plugin support for all other modules
    
    * Add todo for omniauth session controller
    
    * Fix broken specs & add todos/pending specs
    
    * Fix ameba

--- spec/mochi/controllers/omniauthable/user_controller_spec.cr
+++ spec/mochi/controllers/omniauthable/user_controller_spec.cr
@@ -1,6 +1,6 @@
 require "../../../spec_helper"
 
 describe Mochi::Controllers::Omniauthable::UserController do
-  [Mochi::Controllers::Omniauthable::UserController].each do |_controller_class|
-  end
+  # TODO
+  pending "All specs"
 end
